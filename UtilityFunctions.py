12f68f5a9bbcc9a1ec46cf37d1bb99dfd5491362
Jovan Bebic <bebic@ge.com>
2018-07-27 11:01:58 -0400

    FixDST and Convert Feather functions now working

--- UtilityFunctions.py
+++ UtilityFunctions.py
@@ -49,7 +49,7 @@ def FixDST(dirin='./', fnamein='IntervalDataDST.csv',
 
     
     #%% Output header information to log file
-    print('This is: %s, Version: %s' %(codeName, codeVersion))
+    print('\nThis is: %s, Version: %s' %(codeName, codeVersion))
     foutLog.write('This is: %s, Version: %s\n' %(codeName, codeVersion))
     foutLog.write('%s\n' %(codeCopyright))
     foutLog.write('%s\n' %(codeAuthors))
@@ -105,26 +105,31 @@ def FixDST(dirin='./', fnamein='IntervalDataDST.csv',
                     df1.at[ix, 'datetime'] = temp
                     df1.at[ix, 'datetimestr'] = temp.strftime('%d%b%Y:%H:%M').upper()
 
-    print('\nWriting: %s' %os.path.join(dirout,fnameout))
-    foutLog.write('Writing: %s\n' %os.path.join(dirout,fnameout))
     if OutputFormat == 'SCE':
+        print('\nWriting: %s in %s format' %(os.path.join(dirout,fnameout), OutputFormat))
+        foutLog.write('Writing: %s in %s format\n' %(os.path.join(dirout,fnameout), OutputFormat))
         df1.sort_values(by=['CustomerID', 'datetimestr'], inplace = True)
         df1.to_csv(os.path.join(dirout,fnameout), index=False, float_format='%.1f', columns=['datetimestr', 'Demand', 'CustomerID'])
-    else:
+    elif OutputFormat == 'ISO':
+        print('\nWriting: %s in %s format' %(os.path.join(dirout,fnameout), OutputFormat))
+        foutLog.write('Writing: %s in %s format\n' %(os.path.join(dirout,fnameout), OutputFormat))
         df1.set_index(['CustomerID', 'datetime'], inplace=True)
         df1.sort_index(inplace=True) # need to sort on datetime **TODO: Check if this is robust
         # df1.drop(['datetimestr'], axis=1, inplace=True) # drop redundant column
         df1.to_csv(os.path.join(dirout,fnameout), index=True, float_format='%.1f', date_format='%Y-%m-%d %H:%M', columns=['Demand'])
-
+    else:
+        print('\nUnrecognized output format, writing in %s in SCE format' %os.path.join(dirout,fnameout))
+        foutLog.write('\nUnrecognized output format, writing in %s in SCE format\n' %os.path.join(dirout,fnameout))
+        df1.sort_values(by=['CustomerID', 'datetimestr'], inplace = True)
+        df1.to_csv(os.path.join(dirout,fnameout), index=False, float_format='%.1f', columns=['datetimestr', 'Demand', 'CustomerID'])
+        
     logTime(foutLog, '\nRunFinished at: ', codeTstart)
-    print('Finished')
+    print('Finished\n')
     
 def ConvertFeather(dirin='./', fnamein='IntervalData.feather', 
                    dirout='./', fnameout='IntervalData.csv', 
                    dirlog='./', fnameLog='ConvertFeather.log',
-                   tzinput = 'America/Los_Angeles',
                    renameDict={},
-                   CorrectDST = False,
                    writeOutput = False):
     #%% Version and copyright info to record on the log file
     codeName = 'ConvertFeather.py'
@@ -137,7 +142,7 @@ def ConvertFeather(dirin='./', fnamein='IntervalData.feather',
     foutLog = open(os.path.join(dirlog, fnameLog), 'w')
     
     #%% Output header information to log file
-    print('This is: %s, Version: %s' %(codeName, codeVersion))
+    print('\nThis is: %s, Version: %s' %(codeName, codeVersion))
     foutLog.write('This is: %s, Version: %s\n' %(codeName, codeVersion))
     foutLog.write('%s\n' %(codeCopyright))
     foutLog.write('%s\n' %(codeAuthors))
@@ -153,41 +158,6 @@ def ConvertFeather(dirin='./', fnamein='IntervalData.feather',
     if len(renameDict) > 0:
         df1.rename(columns=renameDict, inplace=True)
     
-    if CorrectDST: # Correcting Daylight Savings   
-        # Prepare a dataframe with daylight savings time transitions
-        tz = timezone(tzinput)
-        tzTransTimes = tz._utc_transition_times
-        tzTransInfo = tz._transition_info
-        df2 = pd.DataFrame.from_dict({'DSTbeginsUTC':tzTransTimes[11:-2:2], 'DSTendsUTC':tzTransTimes[12:-1:2],
-                                      'tzinfob'     :tzTransInfo[11:-2:2],  'tzinfoe'   :tzTransInfo[12:-1:2]})
-        df2['year']=pd.DatetimeIndex(df2['DSTbeginsUTC']).year.values
-        df2.set_index('year', inplace=True)
-    
-        # Find the transtion times for the year in df1
-        year = df1['datetimestr'][0].year
-        dst1 = df2['DSTbeginsUTC'][year]+df2['tzinfob'][year][0]
-        dst2 = df2['DSTendsUTC'][year]  +df2['tzinfoe'][year][0]
-        
-        dst1a = dst1 - pd.Timedelta(hours=1)
-        dst2a = dst2 + pd.Timedelta(hours=1)
-        dst2b = dst2 + pd.Timedelta(hours=2)
-        if df1[df1['datetimestr'] == dst1a].empty:
-            # There is a blank record at the start of DST, shift the records to the left
-            df1.loc[(df1['datetimestr'] > dst1a) & (df1['datetimestr'] < dst2b), 'datetimestr'] = df1[(df1['datetimestr'] > dst1a) & (df1['datetimestr'] < dst2b)]['datetimestr'] - pd.Timedelta(hours=1)
-            if (df1[df1['datetimestr'] == dst2]['datetimestr'].size > 1):
-                # There is a double record at the end of DST, move one of them to the right
-                for td in [pd.Timedelta('0 min'), pd.Timedelta('15 min'), pd.Timedelta('30 min'), pd.Timedelta('45 min')]:
-                    print(td)    
-            else:
-                # There is a single record at the end of DST, split value into two and move one to the right
-                for td in [pd.Timedelta('0 min'), pd.Timedelta('15 min'), pd.Timedelta('30 min'), pd.Timedelta('45 min')]:
-                    dmnd = df1[df1['datetimestr']==dst2+td]['Demand'].values[0]/2.
-                    year = df1[df1['datetimestr']==dst2+td]['year'].values[0]
-                    temp = []
-                    temp.insert(0, {'datetimestr': dst2a+td, 'Demand': dmnd, 'year': year})
-                    df1 = pd.concat([pd.DataFrame(temp), df1], ignore_index=True)
-                    df1.loc[(df1['datetimestr'] == dst2+td),'Demand'] = dmnd
-                
     # Add CustomerID
     if not('CustomerID' in df1.columns): df1['CustomerID'] = '1515Grocer'
 
@@ -202,20 +172,19 @@ def ConvertFeather(dirin='./', fnamein='IntervalData.feather',
         df1.to_csv(os.path.join(dirout,fnameout), index=False, float_format='%.1f', columns=['datetimestr', 'Demand', 'CustomerID'])
 
     logTime(foutLog, '\nRunFinished at: ', codeTstart)
-    print('Finished')
+    print('Finished\n')
 
     return
 
 if __name__ == "__main__":
-#    ConvertFeather(dirin='input/', fnamein='GroceryTOU_GS3B_Q1O15_152017.feather',
-#                dirout='input/', fnameout='GroceryTOU_GS3B_Q1O15_152017.csv',
-#                dirlog='output/',
-#                renameDict={'DatePeriod':'datetimestr', 'Usage':'Demand'},
-#                CorrectDST = True,
-#                writeOutput = True)
+    ConvertFeather(dirin='input/', fnamein='GroceryTOU_GS3B_Q1O15_152017.feather',
+                dirout='input/', fnameout='GroceryTOU_GS3B_Q1O15_152017.csv',
+                dirlog='output/',
+                renameDict={'DatePeriod':'datetimestr', 'Usage':'Demand'},
+                writeOutput = True)
 
     FixDST(dirin='input/', fnamein='two_grocers_DST_test.csv', 
                    dirout='output/', fnameout='two_grocers.csv', 
                    dirlog='output/', fnameLog='FixDST.log',
                    tzinput = 'America/Los_Angeles',
-                   OutputFormat = 'SCE')
+                   OutputFormat = 'ISO')

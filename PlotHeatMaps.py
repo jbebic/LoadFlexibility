b9af42cb7362787045903b27ba715e2a6c552c87
berryi <berryi@ge.com>
2018-12-17 12:35:08 -0500

    add billing Heat map functions

--- PlotHeatMaps.py
+++ PlotHeatMaps.py
@@ -22,15 +22,15 @@ import matplotlib.pyplot as plt # plotting
 import matplotlib.backends.backend_pdf as dpdf # pdf output
 
 #%% Importing modules
-from SupportFunctions import getData, logTime, createLog, findUniqueIDs
+from SupportFunctions import getData, logTime, createLog, findUniqueIDs, getDataAndLabels
 
 #%% Version and copyright info to record on the log file
 codeName = 'PlotHeatMaps.py'
-codeVersion = '1.4'
+codeVersion = '1.5'
 codeCopyright = 'GNU General Public License v3.0' # 'Copyright (C) GE Global Research 2018'
 codeAuthors = "Jovan Bebic & Irene Berry, GE Global Research\n"
 
-#%% Function Definitions
+#%% Internal-Facing Function Definitions
 def outputLoadHeatmap(pltPdf, df1,  title, cid, foutLog, weeklyBox=True, varName='NormDmnd'):
     """ creates an annual heatmap with daily bar charts for a single customer"""
     
@@ -71,22 +71,45 @@ def outputLoadHeatmap(pltPdf, df1,  title, cid, foutLog, weeklyBox=True, varName
             weeklyData.append(df2.loc[df2['week']==wk,varName].values)
         
     try:
-        df3 = df2.pivot(index='hour', columns='day', values=varName) 
-        cmax = np.ceil( df3.max().max() * 4 ) / 4
-        im0 = ax0.imshow(df3.iloc[:,:], interpolation='none', #'nearest'
-                                              cmap='viridis', 
-                                              origin='lower', 
-                                              vmin = cmin, 
-                                              vmax = cmax)
         
-        if weeklyBox:
-            ax1.boxplot(weeklyData, manage_xticks = False)
+        if np.min(df2[varName])<-0.01:
+            print('creating delta heatmap')
+            # delta plot
+            df3 = df2.pivot(index='hour', columns='day', values=varName) 
+            cmax = np.ceil( df3.max().max() * 4 ) / 4
+            im0 = ax0.imshow(df3.iloc[:,:], interpolation='none', #'nearest'
+                                                  cmap='Spectral_r', 
+                                                  origin='lower', 
+                                                  vmin = -cmax, 
+                                                  vmax = cmax)
+            
+            if weeklyBox:
+                ax1.boxplot(weeklyData, manage_xticks = False)
+            else:
+                ax1.boxplot(df3.values, manage_xticks = False)
+            ax1.set_ylim([-cmax,cmax]) 
+            ax0.set_aspect('auto')
+            ax1.set_aspect('auto')
+            fig.colorbar(im0, ax=[ax0,ax1])
+            
         else:
-            ax1.boxplot(df3.values, manage_xticks = False)
-        ax1.set_ylim([0,cmax]) 
-        ax0.set_aspect('auto')
-        ax1.set_aspect('auto')
-        fig.colorbar(im0, ax=[ax0,ax1])
+            # min of zero
+            df3 = df2.pivot(index='hour', columns='day', values=varName) 
+            cmax = np.ceil( df3.max().max() * 4 ) / 4
+            im0 = ax0.imshow(df3.iloc[:,:], interpolation='none', #'nearest'
+                                                  cmap='viridis', 
+                                                  origin='lower', 
+                                                  vmin = cmin, 
+                                                  vmax = cmax)
+            
+            if weeklyBox:
+                ax1.boxplot(weeklyData, manage_xticks = False)
+            else:
+                ax1.boxplot(df3.values, manage_xticks = False)
+            ax1.set_ylim([0,cmax]) 
+            ax0.set_aspect('auto')
+            ax1.set_aspect('auto')
+            fig.colorbar(im0, ax=[ax0,ax1])
         
         pltPdf.savefig() # Saves fig to pdf
         plt.close() # Closes fig to clean up memory
@@ -111,19 +134,22 @@ def outputBillingHeatmap(pltPdf, df1,  title, cid, foutLog):
     
     df2 = df1[df1['CustomerID']==cid]
     
-    fig, (ax0, ax1) = plt.subplots(nrows=2, ncols=1,figsize=(8,6),sharex=True)
+    # figure creation & formatting
+    fig, (ax0, ax1) = plt.subplots(nrows=2, ncols=1,figsize=(8,6),sharex=False)
     fig.suptitle(title) # This titles the figure
     plt.subplots_adjust(wspace=0.3,hspace=0.3 )   
     
+    # formatting for upper plot
     ax0.set_title('Energy Cost [c/kWh]') 
     ax0.set_ylabel('Hour')
-#    ax0.set_xlabel('Day of Year')
-    ax0.set_xlim([0,365])
+    ax1.set_xlabel('Day of Year')
+    ax1.set_xlim([0,365])
     ax0.set_ylim([0,24*4])
     ax0.set_yticks(np.linspace(0, 96, num=5).tolist())
     ax0.set_yticklabels(np.linspace(0, 24, num=5, dtype=np.int16).tolist())
     ax0.set_yticklabels(['0', '6', '12', '18', '24'])     
     
+    # formatting for lower plot
     ax1.set_title('Energy Charge [$]') 
     ax1.set_ylabel('Hour')
     ax1.set_xlabel('Day of Year')
@@ -132,26 +158,21 @@ def outputBillingHeatmap(pltPdf, df1,  title, cid, foutLog):
     ax1.set_yticks(np.linspace(0, 96, num=5).tolist())
     ax1.set_yticklabels(np.linspace(0, 24, num=5, dtype=np.int16).tolist())
     ax1.set_yticklabels(['0', '6', '12', '18', '24'])        
-    
 
     cmin = 0.0
     df3 = pd.DataFrame(index=np.arange(0, 24, 0.25), columns=np.arange(0,367))
     df3.iloc[:] = np.nan # resetting all values to nan to prevent backfilling from other customers
-    
-    df2 = df2.assign(week =pd.Series(df2.index.week,index=df2.index))
-    
     try:
         df3 = df2.pivot(index='hour', columns='day', values="EnergyCost") 
         cmax = np.ceil( df3.max().max() * 4 ) / 4
         im0 = ax0.imshow(df3.iloc[:,:], interpolation='none', #'nearest'
-                                              cmap= 'inferno',
+                                              cmap= 'viridis',  
                                               origin='lower', 
                                               vmin = cmin, 
                                               vmax = 12)
         
         ax0.set_aspect('auto')
         fig.colorbar(im0, ax=[ax0])
-        
         
         df4 = df2.pivot(index='hour', columns='day', values="EnergyCharge") 
         cmax = np.ceil( df4.max().max() * 4 ) / 4
@@ -167,8 +188,8 @@ def outputBillingHeatmap(pltPdf, df1,  title, cid, foutLog):
         plt.close() # Closes fig to clean up memory
         successFlag = True
         
-    except:
         
+    except:
         successFlag = False
         foutLog.write("\n*** Unable to create duration plot for %s " %cid )
         print("*** Unable to create duration plot for %s " %cid)
@@ -180,7 +201,111 @@ def outputBillingHeatmap(pltPdf, df1,  title, cid, foutLog):
     
     return successFlag
 
-#%% external-facing fuctions
+
+
+def outputThreeHeatmaps(pltPdf, df1,  title, cid, foutLog):
+    """ creates an annual heatmap with daily bar charts for a single customer"""
+    
+    df2 = df1[df1['CustomerID']==cid]
+    
+    # figure creation & formatting
+    fig, (ax0, ax1, ax2) = plt.subplots(nrows=3, ncols=1,figsize=(8,6),sharex=True)
+#    fig.tight_layout()
+    fig.suptitle(title) # This titles the figure
+    plt.subplots_adjust(wspace=0.3,hspace=0.5 )   
+    
+    # formatting for upper plot
+    ax0.set_title('Price [c/kWh]') 
+    ax0.set_ylabel('Hour')
+    ax0.set_ylim([0,24*4])
+    ax0.set_yticks(np.linspace(0, 96, num=5).tolist())
+    ax0.set_yticklabels(np.linspace(0, 24, num=5, dtype=np.int16).tolist())
+    ax0.set_yticklabels(['0', '6', '12', '18', '24'])     
+    ax0.set_xlim([0,365])
+    
+    # formatting for upper plot
+    ax1.set_title('Demand [MWh]') 
+    ax1.set_ylabel('Hour')
+    ax1.set_ylim([0,24*4])
+    ax1.set_yticks(np.linspace(0, 96, num=5).tolist())
+    ax1.set_yticklabels(np.linspace(0, 24, num=5, dtype=np.int16).tolist())
+    ax1.set_yticklabels(['0', '6', '12', '18', '24'])     
+    ax1.set_xlim([0,365])
+    
+    # formatting for lower plot
+    ax2.set_title('Energy Charge [$]') 
+    ax2.set_ylabel('Hour')
+    ax2.set_ylim([0,24*4])
+    ax2.set_yticks(np.linspace(0, 96, num=5).tolist())
+    ax2.set_yticklabels(np.linspace(0, 24, num=5, dtype=np.int16).tolist())
+    ax2.set_yticklabels(['0', '6', '12', '18', '24'])        
+    ax2.set_xlabel('Day of Year')
+    ax2.set_xlim([0,365])
+    
+    cmin = 0.0
+    df3 = pd.DataFrame(index=np.arange(0, 24, 0.25), columns=np.arange(0,367))
+    df3.iloc[:] = np.nan # resetting all values to nan to prevent backfilling from other customers
+    try:
+        
+        # cost from TOU
+        df3 = df2.pivot(index='hour', columns='day', values="EnergyCost") 
+        cmax = np.ceil( df3.max().max() * 4 ) / 4
+        im0 = ax0.imshow(df3.iloc[:,:], interpolation='none', #'nearest'
+                                              cmap= 'viridis',  
+                                              origin='lower', 
+                                              vmin = cmin, 
+                                              vmax = 12)
+        ax0.set_aspect('auto')
+        fig.colorbar(im0, ax=[ax0])
+        
+        
+        # total charge
+        df5 = df2.pivot(index='hour', columns='day', values="Demand") 
+        cmax = np.ceil( df5.max().max() * 4 ) / 4
+        im0 = ax1.imshow(df5.iloc[:,:], interpolation='none', #'nearest'
+                                              cmap='viridis',  
+                                              origin='lower', 
+                                              vmin = cmin, 
+                                              vmax = cmax)
+        
+        ax1.set_aspect('auto')
+        fig.colorbar(im0, ax=[ax1]) 
+        
+        
+        
+        # total charge
+        df4 = df2.pivot(index='hour', columns='day', values="EnergyCharge") 
+        cmax = np.ceil( df4.max().max() * 4 ) / 4
+        im0 = ax2.imshow(df4.iloc[:,:], interpolation='none', #'nearest'
+                                              cmap='viridis',  
+                                              origin='lower', 
+                                              vmin = cmin, 
+                                              vmax = cmax)
+        
+        ax2.set_aspect('auto')
+        fig.colorbar(im0, ax=[ax2]) 
+        
+        
+        
+        pltPdf.savefig() # Saves fig to pdf
+        plt.close() # Closes fig to clean up memory
+        successFlag = True
+        
+        
+    except:
+        successFlag = False
+        foutLog.write("\n*** Unable to create duration plot for %s " %cid )
+        print("*** Unable to create duration plot for %s " %cid)
+        
+        try:
+            plt.close()    
+        except:
+            pass
+    
+    return successFlag
+
+
+#%% External-Facing Fuctions Definitions
 def PlotHeatMaps(dirin='./', fnamein='IntervalData.normalized.csv', ignoreCIDs='', considerCIDs='',
                  dirout='./', fnameout='HeatMaps.pdf', 
                  dirlog='./', fnameLog='PlotHeatMaps.log',
@@ -190,10 +315,11 @@ def PlotHeatMaps(dirin='./', fnamein='IntervalData.normalized.csv', ignoreCIDs='
 
     # Capture start time of code execution and open log file
     codeTstart = datetime.now()
-    foutLog = createLog(codeName, codeVersion, codeCopyright, codeAuthors, dirlog, fnameLog, codeTstart)
+    foutLog = createLog(codeName, 'PlotHeatMaps', codeVersion, codeCopyright, codeAuthors, dirlog, fnameLog, codeTstart)
     
     # load data from file, find initial list of unique IDs. Update log file
-    df1, UniqueIDs, foutLog = getData(dirin, fnamein, foutLog, varName=varName, usecols=[0,1,column])
+#    df1, UniqueIDs, foutLog = getDataWithLabels(dirin, fnamein, foutLog, varName=varName, usecols=[0,1,column])
+    df1, UniqueIDs, foutLog = getDataAndLabels(dirin, fnamein, foutLog)
     df1['day'] = df1.index.dayofyear
     df1['hour'] = df1.index.hour + df1.index.minute/60.
 
@@ -225,6 +351,49 @@ def PlotHeatMaps(dirin='./', fnamein='IntervalData.normalized.csv', ignoreCIDs='
     
     return
 
+def Plot3HeatMaps(dirin='./', fnamein='IntervalData.normalized.csv', ignoreCIDs='', considerCIDs='',
+                 dirout='./', fnameout='ThreeHeatMaps.pdf', 
+                 dirlog='./', fnameLog='Plot3HeatMaps.log'):    
+    
+    """ Create pdf with one page per customer showing three heat maps: rate, demand, charge """
+
+    # Capture start time of code execution and open log file
+    codeTstart = datetime.now()
+    foutLog = createLog(codeName, "Plot3HeatMaps", codeVersion, codeCopyright, codeAuthors, dirlog, fnameLog, codeTstart)
+    
+    # load data from file, find initial list of unique IDs. Update log file
+    df1, UniqueIDs, foutLog = getData(dirin, fnamein, foutLog, varName=["Demand", "EnergyCharge"], usecols=[0,1,2,3])
+    df1['day'] = df1.index.dayofyear
+    df1['hour'] = df1.index.hour + df1.index.minute/60.
+    df1['EnergyCost'] = df1['EnergyCharge'] / df1['Demand'] * 100
+    
+    # apply ignore and consider CIDs to the list of UniqueIDs. Update log file.
+    UniqueIDs, foutLog = findUniqueIDs(dirin, UniqueIDs, foutLog, ignoreCIDs, considerCIDs)
+
+    print('Opening plot file: %s' %(os.path.join(dirout, fnameout)))
+    foutLog.write('Opening plot file: %s\n' %(os.path.join(dirout, fnameout)))
+    pltPdf1  = dpdf.PdfPages(os.path.join(dirout, fnameout))
+
+    i = 1
+    figN = 0
+    for cID in sorted(list(set(UniqueIDs))):
+        print ('Processing %s (%d of %d) ' %(cID, i, len(UniqueIDs)))
+        i += 1
+        successFlag = outputThreeHeatmaps(pltPdf1, df1,  fnamein+'/'+cID, cID, foutLog)
+        if successFlag:
+            figN += 1
+            
+    foutLog.write('\nNumber of customer IDs for which figures were generated: %d\n' % figN)
+    print('Number of customer IDs for which figures were generated: ' + str( figN))
+
+    # Closing plot files
+    print('Closing plot files')
+    foutLog.write('Closing plot files\n')
+    pltPdf1.close()
+
+    logTime(foutLog, '\nRunFinished at: ', codeTstart)
+    
+    return
 
 def PlotHeatMapOfBilling(dirin='./', fnamein='IntervalData.normalized.csv', ignoreCIDs='', considerCIDs='',
                  dirout='./', fnameout='HeatMaps.pdf', 
@@ -234,13 +403,14 @@ def PlotHeatMapOfBilling(dirin='./', fnamein='IntervalData.normalized.csv', igno
 
     # Capture start time of code execution and open log file
     codeTstart = datetime.now()
-    foutLog = createLog(codeName, codeVersion, codeCopyright, codeAuthors, dirlog, fnameLog, codeTstart)
+    foutLog = createLog(codeName, "PlotHeatMapOfBilling", codeVersion, codeCopyright, codeAuthors, dirlog, fnameLog, codeTstart)
     
     # load data from file, find initial list of unique IDs. Update log file
     df1, UniqueIDs, foutLog = getData(dirin, fnamein, foutLog, varName=["Demand", "EnergyCharge"], usecols=[0,1,2,3])
     df1['day'] = df1.index.dayofyear
     df1['hour'] = df1.index.hour + df1.index.minute/60.
     df1['EnergyCost'] = df1['EnergyCharge'] / df1['Demand'] * 100
+    
     # apply ignore and consider CIDs to the list of UniqueIDs. Update log file.
     UniqueIDs, foutLog = findUniqueIDs(dirin, UniqueIDs, foutLog, ignoreCIDs, considerCIDs)
 
@@ -268,6 +438,7 @@ def PlotHeatMapOfBilling(dirin='./', fnamein='IntervalData.normalized.csv', igno
     logTime(foutLog, '\nRunFinished at: ', codeTstart)
     
     return
+
 if __name__ == "__main__":
     PlotHeatMaps(dirin='output/', fnamein='synthetic2.normalized.csv',
                  dirout='plots/', fnameout='HeatMaps.synthetic2.pdf',

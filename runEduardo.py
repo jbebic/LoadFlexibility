b0b5c17ec651166c5356f9d1a4beb9cda6bc71e8
Jovan Bebic <bebic@ge.com>
2019-08-22 13:16:49 -0400

    Update runEduardo.py

--- runEduardo.py
+++ runEduardo.py
@@ -5,14 +5,22 @@ Created on Thu Aug 22 12:52:38 2019
 @author: 200010679
 """
 
-from NormalizeLoads import NormalizeLoads # ReviewLoads, NormalizeGroup
+from NormalizeLoads import NormalizeLoads, NormalizeGroup # ReviewLoads
 
 if True:
     fnamebase = 'synthetic2_old' # Name your input files here
     ratefile = 'SCE-TOU-GS3-B.csv' # name of TOU rate profile
     considerfname = 'groceryStores_CustomerI.csv'
 
-if True:
+if False:
     NormalizeLoads(dirin='input/', fnamein=fnamebase + '.A.csv', considerCIDs=considerfname, #ignoreCIDs=fnamebase + '.A.ignore.csv',
                    dirout='eduardo/', fnameout=fnamebase + '.A.normalized.csv',
                    dirlog='eduardo/')
+
+if True:
+    groupName='g1L'
+    NormalizeGroup(dirin='input/', fnamein=fnamebase + '.A.csv',
+                           considerCIDs=considerfname + '.' + fnamebase + "." + groupName + ".groupIDs.csv",
+                           demandUnit='kW',
+                           dirout='eduardo/', fnameout=fnamebase + "." + groupName +  '.normalized.csv',
+                           dirlog='eduardo/')    
